534f47ad6134e1b13de12c619aa985043017820b
Matthew Morfopoulos <42120315+mmmorf190@users.noreply.github.com>
2018-10-20 12:34:39 -0500

    move things

---
+++ index.js
@@ -0,0 +1,11 @@
+//Install package via npm install
+// Load Package with require
+var _= require('lodash');
+
+//Use package based on variable we import with
+for (var i=0; i<100; i++){
+var r = _.random(10);
+
+console.log(r);}
+
+//run this in git bash using (node index.html)
